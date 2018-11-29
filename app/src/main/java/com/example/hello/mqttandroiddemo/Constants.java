4ba1fb06f67a2b5c801eefe4238169c085b01465
Kazi md. Saidul <kazi.saidul@cloudwell.co>
2018-11-29 17:51:36 +0600

    mqqt test with blocking ui thread

---
+++ app/src/main/java/com/example/hello/mqttandroiddemo/Constants.java
@@ -0,0 +1,10 @@
+package com.example.hello.mqttandroiddemo;
+
+/**
+ * Created by Kazi Md. Saidul Email: Kazimdsaidul@gmail.com  Mobile: +8801675349882 on 29/11/18.
+ */
+public class Constants {
+    public static final String PUBLISH_TOPIC = "MQTT Examples";
+    public static final String MQTT_BROKER_URL = "tcp://34.238.116.130:1883";
+    public static final String CLIENT_ID = "androidkt";
+}
--- app/src/main/java/com/example/hello/mqttandroiddemo/MainActivity.java
+++ app/src/main/java/com/example/hello/mqttandroiddemo/MainActivity.java
@@ -2,64 +2,173 @@ package com.example.hello.mqttandroiddemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
-import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MqttAndroidClient client;
+    private PahoMqttClient pahoMqttClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+      //  setContentView(R.layout.activity_main);
+        String clientId = MqttClient.generateClientId();
 
-        initilization();
-    }
+        //The URL of the Mosquitto Broker is 192.168.9.100:1883
+        final  MqttAndroidClient client = new MqttAndroidClient(this.getApplicationContext(), Constants.MQTT_BROKER_URL, clientId);
 
-    private void initilization() {
-        String topic        = "MQTT Examples";
-        String content      = "Message from MqttPublishSample sdfsa dfsadf";
-        String content2      = "Kazi";
-        int qos             = 2;
-        String broker       = "tcp://34.238.116.130:1883";
-        String clientId     = "JavaSample";
-        MemoryPersistence persistence = new MemoryPersistence();
+        client.setCallback(new MqttCallbackHandler(client));//This is here for when a message is received
+
+        MqttConnectOptions options = new MqttConnectOptions();
 
         try {
-            MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
-            MqttConnectOptions connOpts = new MqttConnectOptions();
-//            connOpts.setCleanSession(true);
-            System.out.println("Connecting to broker: "+broker);
-            sampleClient.connect(connOpts);
-            System.out.println("Connected");
-            System.out.println("Publishing message: "+content);
+            IMqttToken token = client.connect(options);
+            options.setUserName("fred");
+            options.setPassword("1234".toCharArray());
 
-            MqttMessage message = new MqttMessage(content.getBytes());
-            message.setQos(qos);
-            sampleClient.publish(topic, message);
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // We are connected
+                    Log.d("mqtt", "onSuccess");
+//-----------------------------------------------------------------------------------------------
+                    //PUBLISH THE MESSAGE
+                    MqttMessage message = new MqttMessage("Hello, I am an Android Mqtt Client.".getBytes());
+                    message.setQos(2);
+                    message.setRetained(false);
 
-            for (int i = 0; i < 10; i++) {
-                MqttMessage message1 = new MqttMessage(content2.getBytes());
-                message.setQos(qos);
-                sampleClient.publish(topic, message1);
-            }
+                    String topic = "AndroidPhone";
+
+                    try {
+                        client.publish(topic, message);
+                        Log.i("mqtt", "Message published");
+
+                        // client.disconnect();
+                        //Log.i("mqtt", "client disconnected");
+
+                    } catch (MqttPersistenceException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+
+                    } catch (MqttException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+//-----------------------------------------------------------------------------------------------
+
+                    String subtopic = "tester";
+                    int qos = 1;
+                    try {
+                        IMqttToken subToken = client.subscribe(subtopic, qos);
+                        subToken.setActionCallback(new IMqttActionListener() {
+                            @Override
+                            public void onSuccess(IMqttToken asyncActionToken) {
+                                // The message was published
+                                Log.i("mqtt", "subscription success");
+                            }
+
+                            @Override
+                            public void onFailure(IMqttToken asyncActionToken,
+                                                  Throwable exception) {
+                                // The subscription could not be performed, maybe the user was not
+                                // authorized to subscribe on the specified topic e.g. using wildcards
+                                Log.i("mqtt", "subscription failed");
+
+                            }
+                        });
 
 
 
-            System.out.println("Message published");
-            sampleClient.disconnect();
-            System.out.println("Disconnected");
-            System.exit(0);
-        } catch(MqttException me) {
-            System.out.println("reason "+me.getReasonCode());
-            System.out.println("msg "+me.getMessage());
-            System.out.println("loc "+me.getLocalizedMessage());
-            System.out.println("cause "+me.getCause());
-            System.out.println("excep "+me);
-            me.printStackTrace();
+                    } catch (MqttException e) {
+                        e.printStackTrace();
+                    }
+
+//---------------------------------------------------------------------------
+
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    // Something went wrong e.g. connection timeout or firewall problems
+                    Log.d("mqtt", "onFailure");
+
+                }
+
+            });
+
+
+        } catch (MqttException e) {
+            e.printStackTrace();
         }
+
+    }
+
+
+}//End of Activity class
+
+//-----------------------------------------------------------------------------
+
+class MqttCallbackHandler implements MqttCallbackExtended {
+
+    private final MqttAndroidClient client;
+
+    public MqttCallbackHandler (MqttAndroidClient client)
+    {
+        this.client=client;
+    }
+
+    @Override
+    public void connectComplete(boolean b, String s) {
+        Log.w("mqtt", s);
+    }
+
+    @Override
+    public void connectionLost(Throwable throwable) {
+
+    }
+
+    public void AlarmActivatedMessageReceived()
+    {
+        MqttMessage msg= new MqttMessage("Hello, the Mosquitto Broker got your message saying that the Alarm is Activated.".getBytes());
+        try {
+            this.client.publish("Fitlet", msg);
+            Log.i("mqtt", "Message published");
+
+        } catch (MqttPersistenceException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+
+        } catch (MqttException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+        Log.w("mqtt", mqttMessage.toString());
+
+        if (mqttMessage.toString().contains("Alarm Activated"))
+        {
+            AlarmActivatedMessageReceived();
+        }
+
+    }
+
+    @Override
+    public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
     }
 }
