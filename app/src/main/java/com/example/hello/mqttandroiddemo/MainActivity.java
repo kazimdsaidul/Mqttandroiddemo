56cb8c68650303d93a5e2dcc2c1d6fd7596bde7e
Kazi md. Saidul <kazi.saidul@cloudwell.co>
2018-11-29 16:13:25 +0600

    mqqt test

--- app/src/main/java/com/example/hello/mqttandroiddemo/MainActivity.java
+++ app/src/main/java/com/example/hello/mqttandroiddemo/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.hello.mqttandroiddemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initilization();
+    }
+
+    private void initilization() {
+        String topic        = "MQTT Examples";
+        String content      = "Message from MqttPublishSample sdfsa dfsadf";
+        String content2      = "Kazi";
+        int qos             = 2;
+        String broker       = "tcp://34.238.116.130:1883";
+        String clientId     = "JavaSample";
+        MemoryPersistence persistence = new MemoryPersistence();
+
+        try {
+            MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+//            connOpts.setCleanSession(true);
+            System.out.println("Connecting to broker: "+broker);
+            sampleClient.connect(connOpts);
+            System.out.println("Connected");
+            System.out.println("Publishing message: "+content);
+
+            MqttMessage message = new MqttMessage(content.getBytes());
+            message.setQos(qos);
+            sampleClient.publish(topic, message);
+
+            for (int i = 0; i < 10; i++) {
+                MqttMessage message1 = new MqttMessage(content2.getBytes());
+                message.setQos(qos);
+                sampleClient.publish(topic, message1);
+            }
+
+
+
+            System.out.println("Message published");
+            sampleClient.disconnect();
+            System.out.println("Disconnected");
+            System.exit(0);
+        } catch(MqttException me) {
+            System.out.println("reason "+me.getReasonCode());
+            System.out.println("msg "+me.getMessage());
+            System.out.println("loc "+me.getLocalizedMessage());
+            System.out.println("cause "+me.getCause());
+            System.out.println("excep "+me);
+            me.printStackTrace();
+        }
     }
 }
